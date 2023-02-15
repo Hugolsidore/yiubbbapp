d23c4f72cc58a0244ca69743576395e1aabb5677
Hugolsidore <96905140+Hugolsidore@users.noreply.github.com>
2023-02-15 23:40:19 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app=Flask(__name__) #__name__代表目前執行的模組
 
 @app.route("/") #函式的裝飾 (Decorator): 以函示為基礎,提供附加的功能
 def home():
-    return "Hello Flask 2"
+    return "Hello tpybb"
 
 @app.route("/test") #代表我們要處理的網站路徑
 def test():
