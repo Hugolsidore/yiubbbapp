76e1dfed62138e5a14f6aae01b7a55f341e4ee4a
Hugolsidore <96905140+Hugolsidore@users.noreply.github.com>
2023-02-16 00:11:04 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import render_template
 app=Flask(__name__) #__name__代表目前執行的模組
 
 @app.route("/") #函式的裝飾 (Decorator): 以函示為基礎,提供附加的功能
 def home():
-    return "Hello tpybb"
+    #return "Hello tpybb"
+    return render_template('lovey.html')
 
 @app.route("/test") #代表我們要處理的網站路徑
 def test():
