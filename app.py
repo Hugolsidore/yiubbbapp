7707e5b95ad13b7b686092ebd62bdc68fadd57fc
Hugolsidore <96905140+Hugolsidore@users.noreply.github.com>
2023-02-16 00:39:52 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__) #__name__代表目前執行的模組
 
 @app.route("/") #函式的裝飾 (Decorator): 以函示為基礎,提供附加的功能
 def home():
-    return render_template('lovey.html')
+    return '我愛譚璧瑤'
 
 @app.route("/test") #代表我們要處理的網站路徑
 def test():
