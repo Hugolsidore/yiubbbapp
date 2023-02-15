a6bcd0f9ac59f3312756ba56c0a086bf21b73832
Hugolsidore <96905140+Hugolsidore@users.noreply.github.com>
2023-02-16 01:12:12 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app=Flask(__name__) #__name__代表目前執行的模組
 
 @app.route("/") #函式的裝飾 (Decorator): 以函示為基礎,提供附加的功能
 def home():
-    return render_template('lovey.html')
+    #return render_template('lovey.html')
+    return '<h1>我最愛譚璧瑤了</h1>'
     
 
 @app.route("/test") #代表我們要處理的網站路徑
@@ -12,4 +13,4 @@ def test():
     return "This is Test"
 
 if __name__=="__main__": #如果以主程式執行
-    app.run('0.0.0.0') #立刻啟動伺服器
+    app.run() #立刻啟動伺服器
